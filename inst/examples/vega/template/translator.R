e52e132c27e1aceab1c94ac50f8e415efaf8c1f3
Nacho Caballero <nachocab@gmail.com>
2013-04-04 12:57:06 -0400

    added vega ractive

---
+++ inst/examples/vega/template/translator.R
@@ -0,0 +1,51 @@
+get_spec_path_param <- function(opts) {
+    spec_path <- opts$params$spec_path
+    if (is.null(spec_path)){
+        if (!is.null(opts$params$spec)){
+            spec_path <- file.path(opts$path$data, "spec", paste0(opts$params$spec, ".json"))
+        } else {
+            stop("Please provide a Vega spec name or path (ex: params=list(spec=\"area\") or params=list(spec_path=\"/path/to/spec.json\")")
+        }
+    }
+
+    if (!file.exists(spec_path)){
+        stop("No spec file was found at: ", spec_path)
+    }
+
+    spec_path
+}
+
+get_padding_param <- function(opts) {
+    library(rjson)
+    padding <- opts$params$padding
+    if (is.null(padding)){
+        padding <- c(top = 10, left = 30, bottom = 30, right = 10)
+    }
+
+    if (length(padding) != 4){
+        stop("Please provide four padding values. (currently ", paste(padding, collapse=", "), ")")
+    }
+
+    if (is.null(names(padding))) {
+        names(padding) <- c("top", "left", "bottom", "right")
+    }
+
+    padding <- toJSON(padding)
+    padding
+}
+
+get_data_as_json <- function(opts) {
+    library(df2json)
+    json_data <- df2json(opts$data)
+
+    json_data
+}
+
+get_data_as_json_file <- function(opts) {
+    opts$data <- get_data_as_json(opts)
+    json_file <- create_data_file(opts, "json")
+
+    json_file
+}
+
+
---
+++ inst/examples/vega/tests/test-translator.R
@@ -0,0 +1,65 @@
+context("translate vega")
+
+test_that("input data is translated to the format expected by the template", {
+    test_data_name <- "test_data"
+    opts <- get_opts("vega", params=list(spec="area"), data_name = test_data_name)
+    opts$data <- data.frame(x = c(1, 2, 3), y = c(10, 20, 30))
+    expected_data <- "[{\"x\":1,\"y\":10},\n{\"x\":2,\"y\":20},\n{\"x\":3,\"y\":30}]"
+
+    json_data <- get_data_as_json(opts)
+
+    expect_equal(json_data, expected_data)
+
+    json_file <- get_data_as_json_file(opts)
+    expected_relative_path <- paste("\"", file.path(opts$relative_path$data, paste0(test_data_name, ".json")), "\"")
+    expected_path <- file.path(opts$path$data, paste0(test_data_name, ".json"))
+    expect_true(file.exists(expected_path))
+    expect_equal(readContents(expected_path), expected_data)
+    unlink(expected_path)
+
+    csv_file <- get_data_as_csv_file(opts)
+    expected_relative_path <- paste("\"", file.path(opts$relative_path$data, paste0(test_data_name, ".csv")), "\"")
+    expected_path <- file.path(opts$path$data, paste0(test_data_name, ".csv"))
+    expect_true(file.exists(expected_path))
+    unlink(expected_path)
+
+})
+
+test_that("spec is specified", {
+    data <- ""
+    opts <- get_opts("vega")
+
+    expect_error(get_spec_path_param(opts), "provide a Vega spec")
+
+    expected_spec_path <- file.path(opts$path$data, "spec", "area.json")
+    opts <- get_opts("vega", list(spec_path = expected_spec_path))
+    spec_path <- get_spec_path_param(opts)
+    expect_equal(spec_path, expected_spec_path)
+
+    opts <- get_opts("vega", list(spec_path = "fake_spec.json"))
+    expect_error(get_spec_path_param(opts), "No spec file was found")
+})
+
+test_that("padding is not NULL", {
+    data <- ""
+
+    opts <- get_opts("vega", params=list(spec="area"))
+    padding <- get_padding_param(opts)
+    expect_equal(padding, "{\"top\":10,\"left\":30,\"bottom\":30,\"right\":10}")
+
+    opts <- get_opts("vega", params=list(padding = c(10, 20, 30, 40), spec = "area"))
+    padding <- get_padding_param(opts)
+    expect_equal(padding, "{\"top\":10,\"left\":20,\"bottom\":30,\"right\":40}")
+
+    opts <- get_opts("vega", params=list(padding=c(right=10,bottom=20,left=30,top=40), spec="area"))
+    padding <- get_padding_param(opts)
+    expect_equal(padding, "{\"right\":10,\"bottom\":20,\"left\":30,\"top\":40}")
+
+    opts <- get_opts("vega", params=list(padding=c(10,20,30), spec="area"))
+    expect_error(get_padding_param(opts), "Please provide four padding values")
+})
+
+
+# test_that("extra columns in the df get ignored", {
+
+# })
