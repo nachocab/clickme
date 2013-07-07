cb65a3834ffe40075f42af27afc03e07afa17b5a
Nacho Caballero <nachocab@gmail.com>
2013-07-07 13:43:32 -0400

    improved reload translator documentation

--- R/reload-translators.R
+++ R/reload-translators.R
@@ -1,45 +1,50 @@
-# returns a list of the objects defined by the source files in each path
-get_objects <- function(paths) {
-    functions <- lapply(paths, function(path) {
-        # TODO: handle case when path doesn't exist, no files in path, etc.
 
-        # ensure the main translator file is loaded before the helper files
+#' Traverses a list of paths and determines what objects are defined in the source files that appear in the "translator" folder of each path
+#' It returns a list of sourced objects.
+#' This is done to make control what objects get added to the global namespace
+get_sourced_objects <- function(paths) {
+    sourced_objects <- lapply(paths, function(path) {
         translator_files <- list.files(file.path(path, "translator"), full.names = TRUE)
 
-        # the extra camel_case call is to avoid a weird issue with lowercase folders renamed to uppercase in OSX
+        # the call to camel_case is to avoid a weird issue with lowercase folders renamed to uppercase in OSX
         main_translator_file <- file.path(path, "translator", paste0(camel_case(basename(path)), ".R"))
+
+        # ensure the main translator file is loaded before the helper files
         translator_files <- move_in_front(main_translator_file, translator_files)
 
-        temp <- new.env()
+        temp_environment <- new.env()
         sapply(translator_files, function(translator_file){
-            suppressWarnings(sys.source(translator_file, envir = temp))
+            suppressWarnings(sys.source(translator_file, envir = temp_environment))
         })
 
-        ls(temp)
+        ls(temp_environment)
     })
 
-    names(functions) <- paths
-    functions <- functions[lapply(functions, length) > 0]
+    names(sourced_objects) <- paths
+    # remove empty list elements
+    sourced_objects <- sourced_objects[lapply(sourced_objects, length) > 0]
 
-    functions
+    sourced_objects
 }
 
 
 #' Load the translator files in each template
 #'
-#' Translators are only allowed to create one object with the name of their template in the global namespace.
+#' Translators are only allowed to create one object with the name of their template and a clickme helper function in the global namespace.
 #'
 reload_translators <- function() {
     clickme_helper <<- list()
 
     template_paths <- list.files(getOption("clickme_templates_path"), full.names = TRUE)
-    template_objects <- get_objects(template_paths)
+    template_objects <- get_sourced_objects(template_paths)
 
-    # some paths might have been removed because they didn't contain any objects
+    # some paths might have been removed because they didn't contain any objects, so we ignore them
     template_paths <- names(template_objects)
+
+    # we don't need the paths anymore
     template_objects <- unname(template_objects)
 
-    # Ensure that each translator only creates one object with the name of its template. Optionally, it may also add a helper function as an element of the clickme_helper list.
+    # Ensure that each translator only creates one object with the name of its template and a clickme helper function
     sapply(1:length(template_paths), function(t){
         template_name <- basename(template_paths[t])
 
