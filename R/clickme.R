fa7de856371262e319838d691567c0a3e37509fd
Nacho Caballero <nachocab@gmail.com>
2014-03-21 18:11:57 -0400

    Use cm() instead of clickme(template, ...)

--- R/clickme.R
+++ R/clickme.R
@@ -1,14 +1,25 @@
+#' When called with only the template, it sets the current template used by \code{cm}
+#' When called with more than the template
 #' @export
-clickme <- function(template, ...){
-    reload_translators()
+clickme <- function(template_name, ...){
+    if (length(list(...)) == 0){
+        options(clickme_current_template = template_name)
+    } else {
+        reload_translators()
 
-    template <- as.character(substitute(template))
+        snake_case_template <- snake_case(template_name)
+        camel_case_template <- camel_case(template_name)
 
-    snake_case_template <- snake_case(template)
-    camel_case_template <- camel_case(template)
-
-    if (snake_case_template %notin% names(clickme_helper)){
-        stop(gettextf("\n\n\tThe %s template is missing a helper function or is not installed in %s\n", camel_case_template, getOption("clickme_templates_path")))
+        if (snake_case_template %notin% names(clickme_helper)){
+            stop(gettextf("\n\n\tThe %s template is missing a helper function or is not installed in %s\n", camel_case_template, getOption("clickme_templates_path")))
+        }
+        clickme_helper[[snake_case_template]](...)
     }
-    clickme_helper[[snake_case_template]](...)
+}
+
+
+#' @export
+cm <- function(...){
+    current_template <- getOption("clickme_current_template") %or% "points"
+    clickme(current_template, ...)
 }
