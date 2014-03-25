8a10fc3f12aa530fbd781f6391fb25ece8d2a10a
Nacho Caballero <nachocab@gmail.com>
2014-03-25 19:09:01 -0400

    refactored get_data

--- inst/templates/Lines/translator/Lines-data.R
+++ inst/templates/Lines/translator/Lines-data.R
@@ -41,7 +41,7 @@ lists_to_line_data <- function(x, y, names){
 }
 
 # If x is a vector, convert it to a dataframe
-ensure_dataframe <- function(x, num_rows){
+ensure_is_dataframe <- function(x, num_rows){
     if (!is.null(num_rows) && !is_data_frame_or_matrix(x)){
         x <- matrix(rep(x, num_rows), nrow=num_rows, byrow=T)
     }
@@ -50,24 +50,39 @@ ensure_dataframe <- function(x, num_rows){
 
 Lines$methods(
     get_data = function(){
-        if (is.null(params[["y"]])){
-            if (is_data_frame_or_matrix(params[["x"]])){
-                if (ncol(params[["x"]]) < 2) {
+        data <<- unify_format(params[["x"]], params[["y"]], params$names)
+        # add_names()
+        # add_extra_fields()
+        # cluster_data_by_color_group(params$color_groups, group_variable = "color_group", group_order = internal$ordered_color_group_names)
+
+        # Reverse so the last color group gets the last color
+        # params$palette <<- rev(params$palette)
+
+        # apply_axes_limits()
+        # remove_invalid()
+        # params$tooltip_formats <<- validate_tooltip_formats(params$tooltip_formats)
+    },
+
+    # internal, template-specific
+    unify_format = function(x, y, names) {
+        if (is.null(y)){
+            if (is_data_frame_or_matrix(x)){
+                if (ncol(x) < 2) {
                     stop("When x is a dataframe or a matrix, it must contain at least two columns")
                 }
-                data <<- dataframes_to_line_data(x = params[["x"]],
+                data <- dataframes_to_line_data(x = x,
                                                  y = NULL,
-                                                 names = params$names)
+                                                 names = names)
             } else {
-                data <<- vectors_to_line_data(x = 1:length(params[["x"]]),
-                                              y = params[["x"]],
-                                              name = params$names)
+                data <- vectors_to_line_data(x = 1:length(x),
+                                              y = x,
+                                              name = names)
             }
         } else {
-            if (is_data_frame_or_matrix(params[["x"]]) || is_data_frame_or_matrix(params[["y"]])) {
+            if (is_data_frame_or_matrix(x) || is_data_frame_or_matrix(y)) {
                 # If params$x or params$y is a dataframe, the other must also be a dataframe.
-                x <- ensure_dataframe(params[["x"]], nrow(params[["y"]]))
-                y <- ensure_dataframe(params[["y"]], nrow(params[["x"]]))
+                x <- ensure_is_dataframe(x, nrow(y))
+                y <- ensure_is_dataframe(y, nrow(x))
 
                 if (ncol(x) != ncol(y))
                     stop(sprintf("\nx and y have different number of columns: %s vs. %s",
@@ -76,96 +91,96 @@ Lines$methods(
                 if (ncol(x) < 2)
                     stop("When x is a dataframe or a matrix, it must contain at least two columns")
 
-                data <<- dataframes_to_line_data(x = x,
+                data <- dataframes_to_line_data(x = x,
                                                  y = y,
-                                                 names = params$names)
+                                                 names = names)
             } else {
-                if (is.list(params[["x"]]) && is.list(params[["y"]])){
-                    if (all(sapply(params[["x"]], length) != sapply(params[["y"]], length)))
+                if (is.list(x) && is.list(y)){
+                    if (all(sapply(x, length) != sapply(y, length)))
                         stop(sprintf("\nx and y have different lengths: %s vs. %s",
-                             paste(sapply(params[["x"]], length), collapse = " "),
-                             paste(sapply(params[["x"]], length), collapse = " ")))
-                    data <<- lists_to_line_data(x = params[["x"]],
-                                                y = params[["y"]],
-                                                names = params$names)
+                             paste(sapply(x, length), collapse = " "),
+                             paste(sapply(x, length), collapse = " ")))
+                    data <- lists_to_line_data(x = x,
+                                                y = y,
+                                                names = names)
                 } else {
-                    data <<- vectors_to_line_data(x = params[["x"]],
-                                                  y = params[["y"]],
-                                                  name = params$names)
+                    data <- vectors_to_line_data(x = x,
+                                                  y = y,
+                                                  name = names)
                 }
             }
         }
-
-
-       # data$line_name <<- as.character(params$names %or% rownames(data))
-       # rownames(data) <<- NULL
-
-       # data <<- add_extra_data_fields(data)
-       # data <<- cluster_data_rows(data, params$color_groups, group_variable = "color_group", group_order = internal$ordered_color_group_names)
-
-       # Reverse so the last color group gets the last color
-       # params$palette <<- rev(params$palette)
-
-       # data <<- apply_axes_limits(data)
-       # data <<- na.omit(data)
-       # params$tooltip_formats <<- validate_tooltip_formats(params$tooltip_formats)
+        data
     },
 
+    add_names = function(){
+        data$line_name <<- as.character(params$names %or% rownames(data))
+        rownames(data) <<- NULL
+        return()
+    },
+
+    # internal
     # Add any extra fields to the data object.
     # extra can be a list with as many elements as rows in data, or a data.frame or matrix with as many rows as data
     # This methods should be called before grouping rows
-    add_extra_data_fields = function(x) {
-        x$radius <- params$radius
+    add_extra_fields = function() {
+        data$radius <<- params$radius
         if (!is.null(params$extra)){
-            x <- cbind(x, params$extra)
+            data <<- cbind(x, params$extra)
         }
-
-        x
+        return()
     },
 
+    # internal
     # It makes adjacent rows that belong to the same group
     # groups must have as many elements as data has rows
     # group_variable is the name of the varible that will be used to group the rows
     # group_order must have as many elements as groups, by default the order is
     # alphanumeric
-    cluster_data_rows = function(x, groups, group_variable, group_order = NULL){
-       if (!is.data.frame(x)){
+    cluster_data_by_color_group = function(groups, group_variable, group_order = NULL){
+       if (!is.data.frame(data)){
            stop("\nData must be a dataframe to group its rows")
        }
 
        if (length(groups) > 1){
-           x[[group_variable]] <- as.character(groups)
+           data[[group_variable]] <<- as.character(groups)
 
            if (!is.null(group_order)){
                order <- unlist(sapply(group_order, function(group_name) {
-                   which(x[[group_variable]] == group_name)
+                   which(data[[group_variable]] == group_name)
                }))
            } else {
-               order <- order(x[[group_variable]])
+               order <- order(data[[group_variable]])
            }
 
            # Reverse so the first element is plotted last (and therefore
            # appears on top)
-           x <- x[rev(order),]
+           data <<- data[rev(order),]
        }
-
-       x
+       return()
     },
 
-    apply_axes_limits = function(x) {
+    # internal
+    apply_axes_limits = function() {
        if (!is.null(params$xlim)){
-           x <- x[x$x >= params$xlim[1],]
-           x <- x[x$x <= params$xlim[2],]
+           data <<- data[data$x >= params$xlim[1],]
+           data <<- data[data$x <= params$xlim[2],]
        }
 
        if (!is.null(params$ylim)){
-           x <- x[x$y >= params$ylim[1],]
-           x <- x[x$y <= params$ylim[2],]
+           data <<- data[data$y >= params$ylim[1],]
+           data <<- data[data$y <= params$ylim[2],]
        }
-
-       x
+       return()
     },
 
+    # internal
+    remove_invalid = function(){
+        data <<- na.omit(data)
+        return()
+    },
+
+    # internal
     validate_tooltip_formats = function(tooltip_formats){
         if (any(names(tooltip_formats) %notin% colnames(data))){
             wrong_names <- names(tooltip_formats)[names(tooltip_formats) %notin% colnames(data)]
--- inst/templates/Points/translator/Points-data.R
+++ inst/templates/Points/translator/Points-data.R
@@ -1,80 +1,99 @@
 Points$methods(
 
     get_data = function(){
-       data <<- xy_to_data(params[["x"]], params[["y"]])
-       data$point_name <<- as.character(params$names %or% rownames(data))
-       rownames(data) <<- NULL
+        data <<- unify_format(params[["x"]], params[["y"]])
+        add_names()
+        add_extra_fields()
+        cluster_data_by_color_group(params$color_groups, group_variable = "color_group", group_order = internal$ordered_color_group_names)
 
-       data <<- add_extra_data_fields(data)
-       data <<- cluster_data_rows(data, params$color_groups, group_variable = "color_group", group_order = internal$ordered_color_group_names)
+        # Reverse so the last color group gets the last color
+        params$palette <<- rev(params$palette)
 
-       # Reverse so the last color group gets the last color
-       params$palette <<- rev(params$palette)
-
-       data <<- apply_axes_limits(data)
-       data <<- na.omit(data)
-       params$tooltip_formats <<- validate_tooltip_formats(params$tooltip_formats)
+        apply_axes_limits()
+        remove_invalid()
+        params$tooltip_formats <<- validate_tooltip_formats(params$tooltip_formats)
     },
 
+
+    # internal, template-specific
+    unify_format = function(x, y){
+        data <- xy_to_data(x, y)
+        data
+    },
+
+    # internal
+    add_names = function(){
+        data$point_name <<- as.character(params$names %or% rownames(data))
+        rownames(data) <<- NULL
+        return()
+    },
+
+    # internal
     # Add any extra fields to the data object.
     # extra can be a list with as many elements as rows in data, or a data.frame or matrix with as many rows as data
     # This methods should be called before grouping rows
-    add_extra_data_fields = function(x) {
-        x$radius <- params$radius
+    add_extra_fields = function() {
+        data$radius <<- params$radius
         if (!is.null(params$extra)){
-            x <- cbind(x, params$extra)
+            data <<- cbind(data, params$extra)
         }
-
-        x
+        return()
     },
 
+    # internal
     # It makes adjacent rows that belong to the same group
     # groups must have as many elements as data has rows
     # group_variable is the name of the varible that will be used to group the rows
     # group_order must have as many elements as groups, by default the order is
     # alphanumeric
-    cluster_data_rows = function(x, groups, group_variable, group_order = NULL){
-       if (!is.data.frame(x)){
-           stop("\n\n\tdata must be a dataframe to group its rows")
+    cluster_data_by_color_group = function(groups, group_variable, group_order = NULL){
+       if (!is.data.frame(data)){
+           stop("\nData must be a dataframe to group its rows")
        }
 
        if (length(groups) > 1){
-           x[[group_variable]] <- as.character(groups)
+           data[[group_variable]] <<- as.character(groups)
 
            if (!is.null(group_order)){
                order <- unlist(sapply(group_order, function(group_name) {
-                   which(x[[group_variable]] == group_name)
+                   which(data[[group_variable]] == group_name)
                }))
            } else {
-               order <- order(x[[group_variable]])
+               order <- order(data[[group_variable]])
            }
 
            # Reverse so the first element is plotted last (and therefore
            # appears on top)
-           x <- x[rev(order),]
+           data <<- data[rev(order),]
        }
-
-       x
+       return()
     },
 
-    apply_axes_limits = function(x) {
+    # internal
+    apply_axes_limits = function() {
        if (!is.null(params$xlim)){
-           x <- x[x$x >= params$xlim[1],]
-           x <- x[x$x <= params$xlim[2],]
+           data <<- data[data$x >= params$xlim[1],]
+           data <<- data[data$x <= params$xlim[2],]
        }
 
        if (!is.null(params$ylim)){
-           x <- x[x$y >= params$ylim[1],]
-           x <- x[x$y <= params$ylim[2],]
+           data <<- data[data$y >= params$ylim[1],]
+           data <<- data[data$y <= params$ylim[2],]
        }
-
-       x
+       return()
     },
 
+    # internal
+    remove_invalid = function(){
+        data <<- na.omit(data)
+        return()
+    },
+
+    # internal
     validate_tooltip_formats = function(tooltip_formats){
         if (any(names(tooltip_formats) %notin% colnames(data))){
             wrong_names <- names(tooltip_formats)[names(tooltip_formats) %notin% colnames(data)]
-            stop(sprintf("\n\n\tThe following format names are not x, y, or any of the extra names:\n%s\n\n", enumerate(wrong_names)))
+            stop(sprintf("\nThe following format names are not x, y, or any of the extra names:\n%s\n\n", enumerate(wrong_names)))
         }
 
         tooltip_formats
